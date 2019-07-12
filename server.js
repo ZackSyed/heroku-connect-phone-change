3ede85b64c93ddee9d8aa83643ba54e8e8ef86dd
Zalkifl Syed <zcksyed3@gmail.com>
2019-07-12 16:34:12 -0400

    add salesforce query

--- server.js
+++ server.js
@@ -10,12 +10,34 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.get('/account', function(req, res) {
-    let names = {
-        1: 'counter',
-        2: 'folder'
-    }
-    res.json(names);
-})
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        conn.query(
+            'SELECT Name FROM Account',
+            function(err, result) {
+                if (err != null || result.rowCount == 0) {
+                  conn.query('SELECT Name FROM Account',
+                  function(err, result) {
+                    done();
+                    if (err) {
+                        res.status(400).json({error: err.message});
+                    }
+                    else {
+                        // this will still cause jquery to display 'Record updated!'
+                        // eventhough it was inserted
+                        res.json(result);
+                    }
+                  });
+                }
+                else {
+                    done();
+                    res.json(result);
+                }
+            }
+        );
+    });
+});
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
