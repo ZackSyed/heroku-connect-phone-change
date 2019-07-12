5a2c212290498a57cb8f67d890a4fff95dda0d97
Zalkifl Syed <zcksyed3@gmail.com>
2019-07-12 16:21:30 -0400

    fix configureStore

--- client/src/index.js
+++ client/src/index.js
@@ -6,18 +6,8 @@ import * as serviceWorker from './serviceWorker';
 import configureStore from './store';
 
 document.addEventListener("DOMContentLoaded", () => {
-    let store;
-    if (window.current_user) {
-        const preloadedState = {
-            users: { [window.current_user.id]: window.current_user },
-            session: { id: window.current_user.id }
-        };
-        store = configureStore(preloadedState);
-        delete window.current_user;
-    } else {
-        store = configureStore();
-    }
-
+    let store = configureStore();
+    
     ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
 })
 
